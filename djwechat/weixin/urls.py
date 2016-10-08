cc7b244a094ba432f7ae0570f90813e75c898d05
仲夏叶 <stornado@qq.com>
2016-10-08 22:34:10 +0800

    设置urls

--- djwechat/weixin/urls.py
+++ djwechat/weixin/urls.py
@@ -9,14 +9,14 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+
+from django.conf.urls import url
+from views import WechatMpServer, WechatCorpServer
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^weixin/', include('weixin.urls')),
+    url(r'^mp/(?P<appid>\w+)/$', WechatMpServer.as_view(), name='mp_server'),
+    url(r'^corp/(?P<corpid>\w+)/(?P<agentid>\d+)/$',
+        WechatCorpServer.as_view(), name='corp_server'),
 ]
--- djwechat/weixin/views.py
+++ djwechat/weixin/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.views import View
 # Create your views here.
